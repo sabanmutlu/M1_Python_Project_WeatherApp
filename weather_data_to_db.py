a4b3b4673b0f7ad945a8b7350783cf0db3a682b0
sabanmutlu <68602864+sabanmutlu@users.noreply.github.com>
2022-06-09 23:44:59 +0200

    06.09 3

--- weather_data_to_db.py
+++ weather_data_to_db.py
@@ -1,25 +1,71 @@
-import requests
-import json
-import pandas as pd
+# import requests
+# import json
 import sqlite3
+from sqlite3 import Error
+
+import pandas as pd
 import datetime
-from connect_to_weatherinfo import create_connection
-
-stadt = "Alsdorf"
-land = "Germany"
-api_key = '1f376d57a9f8db239af30093b382b340'
-db_path = "weatherinfo.db"
-
-df = pd.read_json("data.json", orient='index')
-dfc = df.iat[4, 0]
-dfl = pd.json_normalize(dfc)
-dfl.rename(columns={'id': 'city_id', 'name': 'city_name', "coord.lat": "coord_lat", "coord.lon": "coord_lon"},
-           inplace=True)
-dfl["create_date"] = datetime.datetime.now()
-# print(dfl)
-conn = sqlite3.connect(db_path)
-
-dfl.to_sql("weather_request", conn, if_exists='replace', index=False)
-pd.read_sql('select * from weather_request', conn)
 
 
+def create_connection(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_tables(conn):
+    sql_create_weather_request_table = """ CREATE TABLE IF NOT EXISTS weather_request (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        create_date DATETIME,
+        city_id	INTEGER,
+        city_name TEXT,
+        country TEXT,
+        population INTEGER,
+        timezone INTEGER,
+        sunrise INTEGER,
+        sunset INTEGER,
+        coord_lat REAL,
+        coord_lon REAL
+                                    ); """
+    # create tables
+    try:
+        cur = conn.cursor()
+        cur.execute(sql_create_weather_request_table)
+    except Error as e:
+        print(e)
+
+
+def mk_weather_request():
+    # stadt = "Alsdorf"
+    # land = "Germany"
+    # api_key = '1f376d57a9f8db239af30093b382b340'
+    df = pd.read_json("data.json", orient='index')
+    data_city = df.iat[4, 0]
+    df_wr = pd.json_normalize(data_city)
+    df_wr.rename(columns={"id": "city_id", "name": "city_name", "coord.lat": "coord_lat",
+                          "coord.lon": "coord_lon"}, inplace=True)
+    df_wr["create_date"] = datetime.datetime.now()
+    return df_wr
+
+
+def wf_to_sqlite():
+    # Prepare table
+    df_wr = mk_weather_request()
+
+    # Connect database
+    db_path = "weatherinfo.db"
+    conn = create_connection(db_path)
+
+    with conn:
+        # Create table weather_request
+        create_tables(conn)
+        df_wr.to_sql("weather_request", conn, if_exists='append', index=False)
+        see_table = pd.read_sql('select * from weather_request', conn)
+        print(see_table)
+
+
+if __name__ == '__main__':
+    wf_to_sqlite()
